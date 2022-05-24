634c8adf15df3ae3efbb83a67da5956d2d9a2d32
AineeJames <olsenaiden33@gmail.com>
2022-05-24 15:22:38 -0700

    silly git

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import find_packages, setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='cryptcli',
     version='0.1.0',
     license='MIT',
     description='The cryptcli is a python package that gives you access to cryptocurrency prices right in your terminal!',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Aiden Olsen',
     author_email='olsenaiden33@gmail.com',
     url='https://github.com/AineeJames/cryptcli',
+    download_url='https://github.com/AineeJames/cryptcli/releases/tag/v0.1.0',
     keywords=['crypto', 'cli', 'plot'],
     packages=find_packages(),
     install_requires=[
