b9c4dab6ffe00b083bfde69b687a7d11e442deb1
AineeJames <olsenaiden33@gmail.com>
2022-05-24 13:33:06 -0700

    setup config

--- setup.py
+++ setup.py
@@ -2,7 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
     name='cryptcli',
-    version='0.0.0',
+    version='0.1.0',
+    license='MIT',
+    description='The cryptcli is a python package that gives you access to cryptocurrency prices right in your terminal!',
+    author='Aiden Olsen',
+    author_email='olsenaiden33@gmail.com',
+    url='https://github.com/AineeJames/cryptcli',
+    keywords=['crypto', 'cli', 'plot'],
     packages=find_packages(),
     install_requires=[
         'typer',
