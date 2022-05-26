4142a7e8b8b8879c7af54afac98155284975a54d
AineeJames <olsenaiden33@gmail.com>
2022-05-25 21:07:03 -0700

    updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='cryptcli',
-    version='0.2.1',
+    version='0.2.2',
     license='MIT',
     description='The cryptcli is a python package that gives you access to cryptocurrency prices right in your terminal!',
     long_description=long_description,
